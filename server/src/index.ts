e2c48bc8487b8de096fb9d58a9354855019ebae6
Sumitgitup <sumitroy7august@gmail.com>
2025-08-30 14:58:11 +0530

    added basic routing

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,18 @@
 
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/api/health-check', (req, res) => {
+    res.json({
+        message: "API is working fine"
+    })
+});
+
+app.listen(PORT, () => {
+    console.log(`Server on running on http:localhost:${PORT}`);
+})
