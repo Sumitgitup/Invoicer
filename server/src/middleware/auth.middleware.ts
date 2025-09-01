0fac22abcc819c3528b7bd4b430a1cdf0bcbd6a0
Sumitgitup <sumitroy7august@gmail.com>
2025-09-01 18:14:30 +0530

    added middleware

---
+++ server/src/middleware/auth.middleware.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+
+declare global{
+    namespace Express {
+        interface Request {
+            user?: any,
+        }
+    }
+}
+
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const authHeaders = req.headers.authorization;
+    if (!authHeaders || !authHeaders.startsWith('Bearer ')) {
+        return res.status(401).json({ success: false, messsage: 'Unauthorized: NO token provided'})
+    }
+
+    const token = authHeaders.split(' ')[1];
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+
+        req.user = decoded;
+
+        next();
+    } catch (error) {
+        return res.status(401).json({ success: false, message: 'Unauthorized: Invalid token'})
+    }
+}
